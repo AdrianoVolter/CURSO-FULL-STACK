146826bc8230571d138fcb36d35a5c4b80fbc485
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-03 22:03:16 -0300

    Pizzaria do joao

---
+++ pizzaria/script.js
@@ -0,0 +1,42 @@
+function inserirAdicionais(){
+    var massas = document.getElementById('massas');
+    var bordas = document.getElementById('bordas');
+
+    var massasValue = massas.options[massas.selectedIndex];
+    var bordasValue = bordas.options[bordas.selectedIndex];
+
+    var textMassas = massasValue.text;
+    var textBordas = bordasValue.text;
+
+    localStorage.setItem("Massa", JSON.stringify(textMassas));
+    localStorage.setItem("Bordas", JSON.stringify(textBordas));
+}
+
+var sabores =  JSON.parse((localStorage.getItem("sabores"))) || [];
+
+var elLista = document.getElementById("lista");
+var elCampo = document.getElementById("campo");
+var elBotao = document.getElementById("botao");
+
+elCampo.onclick = function() {
+    var nome = elCampo.value;
+    sabores.push({pizza: nome})
+
+     salvarSabores()
+
+    elCampo.value = "";
+
+    
+}
+
+function salvarSabores(){
+    if(sabores.length > 3 ){
+        alert("permitido apenas 3 sabores")
+    }else{
+        localStorage.stringify("sabores", JSON.stringify(sabores));
+    }
+
+
+}
+
+inserirAdicionais()
